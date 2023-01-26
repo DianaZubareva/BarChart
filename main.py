3721ee5e9e15cc74ca9581060d5b05651b5a162f
dianazzz <123642884+DianaZubareva@users.noreply.github.com>
2023-01-26 13:34:31 +0000

    Deleted extra spaces in the list.csv file

--- main.py
+++ main.py
@@ -24,74 +24,38 @@
 
   # |-------------------------------------------| 
 
- 
-
 # X-label - name of mbti type  
-
 # Y-axis - % for average, men & women 
-
 # Use a legend - use ply.plot  
 
-  
-
 import matplotlib.pyplot as ply 
-
 import numpy as np 
 
-  
-
 # average = [14, 12, 12, 9, 9]  
-
 # mbti = ["ISFJ", "ESFJ", "ISTJ", "ISFP", "ESTJ"]  
-
 # men = [8, 8, 16, 8, 11] 
-
 # women = [19, 17, 7, 10, 6] 
 
-  
-
 file = open("list.csv","r") 
-
 dataIn = file.read()  
-
 file.close() 
-
-  
 
 arrays = dataIn.split('\n') 
 
-  
-
-average = [eval(i) for i in arrays[0].split(' ')] 
-
+average = [eval(i) for i in arrays[0].split(' ')]
 mbti = arrays[1].split(' ') 
-
 men = [eval(i) for i in arrays[2].split(' ')] 
-
 women = [eval(i) for i in arrays[3].split(' ')] 
 
-   
-
-  
-
 # Create a line graph with names on x-axis and numbers on  y-axis  
-
-  
 
 X = np.arange(5) 
 
 ply.bar(X - 0.25, list(men), width=0.2)  
-
 ply.bar(list(mbti), list(average), width=0.2)  
-
 ply.bar(X + 0.25, list(women), width=0.2) 
 
-  
-
 ply.xlabel("MBTI")  
-
 ply.ylabel("Total")  
-
 ply.legend(["Men %", "Average %", "Women %"])  
-
 ply.show() 
